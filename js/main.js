2790ff0705d793bae3662dfce8fe4dc913457876
Abdelmoaiz <97130184+Abdelmoaiz@users.noreply.github.com>
2023-03-11 15:40:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let goPage2 = document.querySelector('.goPage2');
+
+goPage2.onclick = function() {
+    document.querySelector('.view').style.display = 'none';
+    document.querySelector('.sign-in').style.display = 'block';
+}
+
---
+++ pages/registes/js/main.js
@@ -0,0 +1,8 @@
+let btnInsertName = document.querySelector('.btnInsertName');
+
+// btnInsertName.onclick = function() {
+//     if(document.querySelector('#insertName').value != '') {
+//         document.querySelector('.question1').style.display = 'none';
+//         document.querySelector('.question2').style.display = 'block';
+//     }
+// }
